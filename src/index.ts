e5b16be5881727df4c6004449406f9e462e2c888
Barış Atay <barisatay0@hotmail.com>
2025-01-07 14:38:01 +0300

    *

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import {serve} from '@hono/node-server';
 import {Hono} from 'hono';
 import './controllers/itemController';
-import {itemRoute} from './route/itemRoute.js';
-import {categoryRoute} from './route/categoryRoute.js';
-import {companyRoute} from "./route/companyRoute.js";
-import {photoRoute} from "./route/photoRoute.js";
+import { itemRoute, categoryRoute, companyRoute, photoRoute } from './route/index.js';
+
+
 
 const app = new Hono();
 
@@ -13,19 +12,12 @@ app.route('/categories', categoryRoute);
 app.route('/company', companyRoute);
 app.route('/photo', photoRoute);
 
-app.get('/', (c) => {
-    return c.text('Hello Hono!');
-});
-app.notFound((c) => {
-    return c.text('Page is not defined', 404);
-});
+app.get('/', (c) => {return c.text('Hello Hono!');});
+app.notFound((c) => {return c.text('Page is not defined', 404);});
 
 const port = 3000;
 console.log(`Server is running on http://localhost:${port}`);
 
-serve({
-    fetch: app.fetch,
-    port
-});
+serve({fetch: app.fetch, port});
 
 export default app;
---
+++ src/route/index.ts
@@ -0,0 +1,6 @@
+import { itemRoute } from './itemRoute.js';
+import { categoryRoute } from './categoryRoute.js';
+import { companyRoute } from "./companyRoute.js";
+import { photoRoute } from "./photoRoute.js";
+
+export { photoRoute, itemRoute, categoryRoute, companyRoute };
